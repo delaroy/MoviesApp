4fd1699f4789f091b84da7db7c6d2c1852890a65
Delaroy Studios <bamoyk@yahoo.com>
2018-09-21 12:06:44 +0100

    enhance_settings

--- app/src/main/java/com/delaroystudios/movieapp/SettingsActivity.java
+++ app/src/main/java/com/delaroystudios/movieapp/SettingsActivity.java
@@ -3,23 +3,18 @@ package com.delaroystudios.movieapp;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by delaroy on 5/18/17.
  */
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
-    }
-
-    public static class SettingsFragment extends PreferenceFragment{
-        @Override
-        public void onCreate(final Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-        }
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
---
+++ app/src/main/java/com/delaroystudios/movieapp/SettingsFragment.java
@@ -0,0 +1,48 @@
+package com.delaroystudios.movieapp;
+
+/**
+ * Created by delaroy on 9/12/18.
+ */
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+    @Override
+    public void onCreate(final Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        Preference orderBy = findPreference(getString(R.string.pref_sort_order_key));
+        bindPreferenceSummaryToValue(orderBy);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                CharSequence[] labels = listPreference.getEntries();
+                preference.setSummary(labels[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+        return true;
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+        SharedPreferences preferences =
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+        String preferenceString = preferences.getString(preference.getKey(), "");
+        onPreferenceChange(preference, preferenceString);
+    }
+}
